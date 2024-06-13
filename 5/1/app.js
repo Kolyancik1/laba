809d851d0a9910b005f5b2efb249800174fcb4ce
Kolyancik1 <imanaevnikolaj787@gmail.com>
2024-06-13 15:04:39 +0300

    Add files via upload

---
+++ 5/1/app.js
@@ -0,0 +1,10 @@
+﻿
+// Переменные
+let age = "53";
+let name = "King";
+
+// Применение переменных
+let output = <span>{name} is {age} years old</span>;
+
+// отрисовка
+ReactDOM.render(output, document.querySelector("#myDiv"));  
---
+++ 5/7/app.js
@@ -0,0 +1,5 @@
+﻿const PrintHello = () => {
+    return <h1>I Love React</h1>;
+}
+
+ReactDOM.render(PrintHello(), myDiv);
